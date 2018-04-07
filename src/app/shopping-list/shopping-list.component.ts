2eb280cf41aae030fb4f9d91709013124ab18939
bootcamp <bootcamp@suportes-MacBook-Pro-2.local>
2018-04-07 13:04:48 -0300

    V4 - service injection

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,27 +10,30 @@ export class ShoppingListComponent implements OnInit {
   
   private listItems: Array<any>;
 
-  constructor() {
-    this.listItems = [
-    {
-      name: 'Bread',
-      disabled: false
-    },
-    {
-      name: 'Butter',
-      disabled: false
-    },
-    {
-      name: 'Coffee',
-      disabled: false
-    },
-    {
-      name: 'Cookies',
-      disabled: true
-    }];
-   }
+  private itemToAdd: string = '';
+
+  constructor(
+    private myShoppingListService: ShoppingListService
+  ) 
+  {
+    this.listItems = this.myShoppingListService.findAll();
+  }
 
   ngOnInit() {
+  }
+
+  private addObjectToList()
+  {
+    // create
+    let newItem = 
+      {
+        name: this.itemToAdd,
+        disabled: false
+      };
+    // add
+    this.myShoppingListService.add(newItem);
+    // clear
+    this.itemToAdd = '';
   }
 
 }
